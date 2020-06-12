9214b0356e06d70f8b5fbe4ef62832c74e2dc9fa
Himanshu Patel <hpatel27299@gmail.com>
2020-06-12 19:50:25 +0530

    WebApp v0.1

--- web_app/app.py
+++ web_app/app.py
@@ -1,84 +1,51 @@
-from __future__ import division, print_function
-# coding=utf-8
-import sys
-import os
-import glob
-import re
-import numpy as np
 
-# Keras
-from keras.applications.imagenet_utils import preprocess_input, decode_predictions
-from keras.models import load_model
-from keras.preprocessing import image
-
-# Flask utils
-from flask import Flask, redirect, url_for, request, render_template
-from werkzeug.utils import secure_filename
-from gevent.pywsgi import WSGIServer
-
-# Define a flask app
-app = Flask(__name__)
-
-# Model saved with Keras model.save()
-MODEL_PATH = 'models/model_resnet.h5'
-
-# Load your trained model
-model = load_model(MODEL_PATH)
-model._make_predict_function()          # Necessary
-# print('Model loaded. Start serving...')
-
-# You can also use pretrained model from Keras
-# Check https://keras.io/applications/
-#from keras.applications.resnet50 import ResNet50
-#model = ResNet50(weights='imagenet')
-#model.save('')
-print('Model loaded. Check http://127.0.0.1:5000/')
-
-
-def model_predict(img_path, model):
-    img = image.load_img(img_path, target_size=(224, 224))
-
-    # Preprocessing the image
-    x = image.img_to_array(img)
-    # x = np.true_divide(x, 255)
-    x = np.expand_dims(x, axis=0)
-
-    # Be careful how your trained model deals with the input
-    # otherwise, it won't make correct prediction!
-    x = preprocess_input(x, mode='caffe')
-
-    preds = model.predict(x)
-    return preds
-
-
-@app.route('/', methods=['GET'])
-def index():
-    # Main page
-    return render_template('index.html')
-
-
-@app.route('/predict', methods=['GET', 'POST'])
-def upload():
-    if request.method == 'POST':
-        # Get the file from post request
-        f = request.files['file']
-
-        # Save the file to ./uploads
-        basepath = os.path.dirname(__file__)
-        file_path = os.path.join(
-            basepath, 'uploads', secure_filename(f.filename))
-        f.save(file_path)
-
-        # Make prediction
-        preds = model_predict(file_path, model)
-
-        # Process your result for human
-        # pred_class = preds.argmax(axis=-1)            # Simple argmax
-        pred_class = decode_predictions(preds, top=1)   # ImageNet Decode
-        result = str(pred_class[0][0][1])               # Convert to string
-        return result
-    return None
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+from flask import Flask, render_template, request 
+from werkzeug.utils import secure_filename 
+from tensorflow.keras.preprocessing.image import ImageDataGenerator 
+import tensorflow as tf 
+import numpy as np 
+import os  
+  
+try: 
+    import shutil 
+    shutil.rmtree('upload / image') 
+    # % cd upload % mkdir image % cd .. 
+    print() 
+except: 
+    pass
+  
+model = tf.keras.models.load_model('model/GoogLeNet.h5') 
+app = Flask(__name__) 
+  
+app.config['UPLOAD_FOLDER'] = 'upload / image'
+  
+@app.route('/') 
+def upload_f(): 
+    return render_template('index.html') 
+  
+def finds(): 
+    test_datagen = ImageDataGenerator(rescale = 1./255) 
+    vals = ['A', 'D', 'C', 'B'] # change this according to what you've trained your model to do 
+    test_dir = 'uploaded'
+    test_generator = test_datagen.flow_from_directory( 
+            test_dir, 
+            target_size =(224, 224), 
+            # color_mode ="rgb", 
+            shuffle = False, 
+            class_mode ='categorical', 
+            batch_size = 1) 
+  
+    pred = model.predict_generator(test_generator) 
+    print(pred) 
+    return str(vals[np.argmax(pred)]) 
+  
+@app.route('/uploader', methods = ['GET', 'POST']) 
+def upload_file(): 
+    if request.method == 'POST': 
+        f = request.files['file'] 
+        f.save(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(f.filename))) 
+        val = finds() 
+        return render_template('pred.html', ss = val) 
+  
+if __name__ == '__main__': 
+    app.run() 
